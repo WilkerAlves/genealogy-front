6ca95544a5f815d305805ab5aee202db8edbd062
WilkerAlves <wilkeralves97@gmail.com>
2023-03-28 11:49:45 -0300

    ajuste na funcionalidade

--- src/components/project/PersonCard.js
+++ src/components/project/PersonCard.js
@@ -1,7 +1,6 @@
-import { Link } from 'react-router-dom'
 import styles from './PersonCard.module.css'
 
-import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+import { BsFillTrashFill } from 'react-icons/bs'
 
 function PersonCard({ id, name, handleRemove }) {
   const remove = (e) => {
@@ -13,9 +12,6 @@ function PersonCard({ id, name, handleRemove }) {
     <div className={styles.person_card}>
       <h4>{name}</h4>
       <div className={styles.person_card_actions}>
-        <Link to={'/persons/' + id}>
-          <BsPencil /> Editar
-        </Link>
         <button onClick={remove}>
           <BsFillTrashFill />
           Excluir
